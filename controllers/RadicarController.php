98aeff7becc936fc54093423639da4fd8307f399
Giovanny Franco <giovannyfranco@mattelsa.net>
2019-04-04 11:57:38 -0500

    Solucionar requisitos

--- controllers/RadicarController.php
+++ controllers/RadicarController.php
@@ -155,6 +155,96 @@
             } 
         }
 
+        /**
+         * Retorna los requisitos asignados a la persona que esta en la sessión
+         * @param Array $requisitos
+         */
+        public function getMisrequisitos() {
+            
+            $idUser = $_SESSION['IDEMPLEADO'];
+
+            $query = "SELECT  
+                `REQ`.IDREQ AS IDREQ,
+                `ESTADO`.`NOMBRE` AS ESTADO,
+                `DET`.IDDETALLEREQ AS IDDETALLE,
+                `DET`.FECHA AS FECHA,
+                `AREA`.`IDAREA` AS IDAREA,
+                `AREA`.`NOMBRE` AS NOMBRE_AREA,
+                `EMPLEADO`.`IDEMPLEADO` AS IDEMPLEADO,
+                `EMPLEADO`.`NOMBRE` AS NOMBRE_EMPLEADO
+            FROM `REQUISITO` AS `REQ`
+            INNER JOIN `ESTADO` ON `ESTADO`.`IDESTADO` = `REQ`.FKESTADO
+            INNER JOIN `DETALLEREQ` AS `DET` ON `DET`.FKREQ = `REQ`.IDREQ
+            INNER JOIN `AREA` ON `AREA`.`IDAREA` = `REQ`.FKAREA
+            INNER JOIN `EMPLEADO` ON `EMPLEADO`.`IDEMPLEADO` = `DET`.FKEMPLEASIG
+            WHERE `REQ`.FKESTADO = 2 AND `EMPLEADO`.`IDEMPLEADO` = ".$idUser.";";
+            
+            $sth = $this->db->prepare($query);
+            $resultado = $sth->execute();
+            $requisitos = [];
+            if($resultado !== false){
+                $requisitos = $sth->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            return $requisitos;
+        }
+
+        /**
+         * Retorna los estados para solucionar un requisito
+         * @param Array $estados
+         */
+        public function getEstados() {
+            
+            $query = "SELECT * FROM ESTADO WHERE IDESTADO IN(3,4)";
+            $sth = $this->db->prepare($query);
+            $resultado = $sth->execute();
+
+            $estados = [];
+            if($resultado !== false){
+                $estados = $sth->fetchAll(PDO::FETCH_ASSOC);
+            }
+            return $estados;
+        }
+
+        public function solucionarRequisitos($data) {
+            if(!empty($data)) {
+                try {
+                    $this->db->beginTransaction();
+                    
+                    $fecha = date('Y-m-d h:i:s');
+                    $estado = $data['radicado-estado']; //Estado Asignado
+                    $texto = $data['radicado-text']; //Texto que ingreso el usuario
+                    $id_empleado = $_SESSION['IDEMPLEADO']; //Id del empleado en la sessión actual
+                    
+                    foreach( $data['requisitos'] as $key => $req) {
+                        
+                        //Cambiamos el estado del requisito
+                        $query = "UPDATE `REQUISITO` SET `FKESTADO`= ".$estado." WHERE `IDREQ`= ".$key." ";
+                        $sth = $this->db->prepare($query);
+                        $resultado = $sth->execute();
+                        $sth->closeCursor();
+
+                        // Insertamos el detalle del requisito
+                        $query = "INSERT INTO DETALLEREQ (FECHA, OBSERVACION, FKEMPLE, FKREQ, FKESTADO, FKEMPLEASIG) 
+                        VALUES ('".$fecha."', '".$texto."', ".$id_empleado.", ".$key.", ".$estado.", ".$id_empleado.")";
+                        $sth = $this->db->prepare($query);
+                        $resultado = $sth->execute();
+                        $sth->closeCursor();
+                        
+                        // commit the transaction
+                        if($this->db->commit()) {
+                            header("Location:../layouts/solucion_requisitos.php?message=Se guardaron todos los cambios.");
+                        }else {
+                            header("Location:../layouts/solucion_requisitos.php?message=No se efectuo el commit.");
+                        }
+                    }
+                } catch (PDOException $e) {
+                    $this->db->rollBack();
+                    header("Location:../index.php?message=".$e->getMessage());
+                }
+            }
+        }
+
     }
 
     $radicar = new Radicar($objetoPDO);
@@ -164,5 +254,8 @@
         }
         if(isset($_GET['list'])) {
             $radicar->asignarRequisitos($_POST);
+        }
+        if(isset($_GET['solu'])) {
+            $radicar->solucionarRequisitos($_POST);
         }
     }
--- layouts/radicar_requisito.php
+++ layouts/radicar_requisito.php
@@ -6,71 +6,73 @@
 
     $radicar = new Radicar($objetoPDO);
 ?>
-<div class="container" style="margin-top: 80px;">
-    <div class="row card">
-        <?php if(isset($_GET['message'])): ?>
-            <div class="card-panel teal lighten-2" id="card-response-message">
-                <p class="white-text" id="response-message"><?= $_GET['message'] ?></p>
-                <i class="tiny material-icons white-text" id="clear-message" onclick="clearMessage()">clear</i>
-            </div>
-        <?php endif;?>
-        <form class="col s12" style="padding: 50px;" id="form-radicar" name="form-radicar" action="../controllers/RadicarController.php?create=1" method="post">
-            <div class="row center">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">mode_edit</i>
-                    <textarea id="icon_prefix2" class="materialize-textarea" name="radicado-text"></textarea>
-                    <label for="icon_prefix2">Radicar requisito: Mensaje</label>
+<main>
+    <div class="container" style="margin-top: 80px;">
+        <div class="row card">
+            <?php if(isset($_GET['message'])): ?>
+                <div class="card-panel teal lighten-2" id="card-response-message">
+                    <p class="white-text" id="response-message"><?= $_GET['message'] ?></p>
+                    <i class="tiny material-icons white-text" id="clear-message" onclick="clearMessage()">clear</i>
                 </div>
-            </div>
-            <div class="row center">
-                <div class="input-field col s12 m6 center">
-                    <select name="radicado-area">
-                        <option value="" disabled selected>Selecciona el área</option>
-                        <?php foreach($radicar->obtenerAreas() as $key => $area):?>
-                            <option value="<?= $area['IDAREA'];?>"> <?= utf8_encode($area['NOMBRE']); ?> </option>
-                        <?php endforeach; ?>
-                    </select>
-                    <label>Área Requisito</label>
+            <?php endif;?>
+            <form class="col s12" style="padding: 50px;" id="form-radicar" name="form-radicar" action="../controllers/RadicarController.php?create=1" method="post">
+                <div class="row center">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">mode_edit</i>
+                        <textarea id="icon_prefix2" class="materialize-textarea" name="radicado-text"></textarea>
+                        <label for="icon_prefix2">Radicar requisito: Mensaje</label>
+                    </div>
                 </div>
-                <div class="input-field col s12 m6 center">
-                    <button class="btn waves-effect waves-light" type="submit" name="action">Radicar
-                        <i class="material-icons right">send</i>
-                    </button>
+                <div class="row center">
+                    <div class="input-field col s12 m6 center">
+                        <select name="radicado-area">
+                            <option value="" disabled selected>Selecciona el área</option>
+                            <?php foreach($radicar->obtenerAreas() as $key => $area):?>
+                                <option value="<?= $area['IDAREA'];?>"> <?= utf8_encode($area['NOMBRE']); ?> </option>
+                            <?php endforeach; ?>
+                        </select>
+                        <label>Área Requisito</label>
+                    </div>
+                    <div class="input-field col s12 m6 center">
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Radicar
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
 
-<script>
-    document.addEventListener('DOMContentLoaded', (event) => { 
-        var elems = document.querySelectorAll('select');
-        var instances = M.FormSelect.init(elems);
-    }, false);
-    
-    /**
-     * Función para ocultar el mensaje que llegué al crear una notificación
-     */
-    function clearMessage() {
-        var s = document.getElementById('card-response-message').style;
-        s.opacity = 1;
-        removeParam();
-        (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
-        return true;
-    }
-    /**
-     * Función para remover los parametro que lleguen por url
-     */
-    function removeParam()
-    {
-        var url = document.location.href;
-        var urlparts = url.split('?');
-        if (urlparts.length >= 1) {
-            var urlBase = urlparts.shift(); 
-            var queryString = urlparts.join("?"); 
-            window.history.pushState('',document.title,urlBase); //Se agrega la url base a la url del navegador
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => { 
+            var elems = document.querySelectorAll('select');
+            var instances = M.FormSelect.init(elems);
+        }, false);
+        
+        /**
+        * Función para ocultar el mensaje que llegué al crear una notificación
+        */
+        function clearMessage() {
+            var s = document.getElementById('card-response-message').style;
+            s.opacity = 1;
+            removeParam();
+            (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
+            return true;
         }
-        return true;
-    }
-    
-</script>
+        /**
+        * Función para remover los parametro que lleguen por url
+        */
+        function removeParam()
+        {
+            var url = document.location.href;
+            var urlparts = url.split('?');
+            if (urlparts.length >= 1) {
+                var urlBase = urlparts.shift(); 
+                var queryString = urlparts.join("?"); 
+                window.history.pushState('',document.title,urlBase); //Se agrega la url base a la url del navegador
+            }
+            return true;
+        }
+        
+    </script>
+</main>
